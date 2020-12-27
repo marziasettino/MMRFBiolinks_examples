27bf57fc71c6a407f3a7d9cafa47ff850be37530
marziasettino <marzia.settino@gmail.com>
2020-12-27 20:25:59 +0100

    Add Case Study 3

--- R/CaseStudy_3.R
+++ R/CaseStudy_3.R
@@ -15,35 +15,44 @@ library(MMRFBiolinks)
 
 
 # Once this is done, you should import them into your R environmnet for feeding the following Workflow.
-# For feeding the workflow with downloaded data, you should leave commented <get(load("data/CaseStudy3.rda"))> .
+# For feeding the workflow with downloaded data, you should leave commented <get(load("data/CaseStudy3.rda"))> and
+# <variant>.
 
 #Note1: pay attention to import MMRF-RG dataset into R environment including the heading of columns.
 
 
-# IMPORTANT 2! If you want initially just test the proposed MMRFBiolinks functions without using MMRF Researcher Gateway (RG) clinical data,
-# you can use fictitious data that we provide you in <clinMMGateway> and you should uncomment <listSamples>.
+# IMPORTANT 2! If you want initially just test the proposed MMRFBiolinks functions without using MMRF Researcher Gateway (RG) data,
+# you can use fictitious data that we provide you in <CaseStudy3.rda> and you should uncomment <get(load("data/CaseStudy3.rda"))> and
+# <variant>.
+
 
  
-get(load("data/CaseStudy3.rda")) 
+#get(load("data/CaseStudy3.rda")) 
+
+#variant <- c("rs755588843", "rs569344016","rs2066497")
+
  
+variant.ann<- read.csv("~/MMRF_CoMMpass_IA14a_All_Canonical_Variants.csv")
+trt<- read.csv("~/MMRF_CoMMpass_IA14_STAND_ALONE_TRTRESP.csv")
+patient<- read.csv("~/MMRF_CoMMpass_IA14_PER_PATIENT.csv")
 
 
 summary.var<-MMRF_RG_VariantCountPlot(variant.ann,trt,topN=50,filenm=NULL)
 
+#We select in summary.var the top four dbSNP ID with the high count related to "Complete Response". 
 
-variant <- c("rs755588843", "rs569344016","rs2066497")
+variant <- c("rs111362472", "rs181828141","rs2157615","rs2157615")
 
-
+#We select in "patient" dataframe only the samples having variants in "variant".
 
 patient.var<-MMRF_RG_GetIDSamplebyVariant(variant.ann,patient,variant)
 
-
+#We perform Survival analysis focused on patients having those variants.
 
 
 MMRF_RG_SurvivalKM(patient.var,
                    trt,
                    FilterBy="treatment", 
                    filename=NULL,
-                   xlim = c(100,3000),
                    conf.range = FALSE,
                    color = c("Dark2"))
